b7025444106c5603a361665e94073d4c1bcec34b
Erick <erick.kine2012@hotmail.com>
2026-05-11 18:37:25 -0300

    feat: add mobile app basic structure

---
+++ mobile/lib/main.dart
@@ -0,0 +1,28 @@
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ISP Tracker',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('ISP Tracker')),
+      body: const Center(child: Text('App Mobile em desenvolvimento')),
+    );
+  }
+}
